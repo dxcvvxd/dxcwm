b5897a60dd806fec39041b926b8795152c55f4c4
dxcvvxd <antonsmunk@gmail.com>
2025-01-21 20:05:13 +0300

    font

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *rofi[]={"rofi","-modi", "drun", "-show", "drun", "-show-icons", NULL};
+static const char *rofi[]={"rofi","-modi", "drun", "-show", "drun", "-show-icons", "-font", "Monocraft Nerd Font 12", NULL};
 static const char *flame[]={"flameshot", "gui", NULL};
 static const char *flamefull[]={"flameshot", "full", "-c", "-p", "/home/dxc/Pictures/screens/", NULL};
 static const char *volup[]={"pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",NULL};
