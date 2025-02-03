a5e1d5e15aa70e4183a4c8ba60c1d1ce112a125d
dxcvvxd <antonsmunk@gmail.com>
2025-02-03 12:25:07 +0300

    xarchiver

--- config.h
+++ config.h
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "xarchiver",NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
 	{ "Kitty",    NULL,       NULL,       0,            0,           -1 },
 	{ "mpv",      NULL,       NULL,       0,            1,           -1 },
